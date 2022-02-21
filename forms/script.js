31df1c9f0ec608f2baa0665a843424bd6ff2ad9a
Matheus Paiva <matheus.paivaads@gmail.com>
2022-02-21 14:55:37 -0300

    arquivos concluidos

---
+++ forms/script.js
@@ -0,0 +1,24 @@
+function responder(event){
+event.preventDefault()
+}
+window.onload = function(){
+    const btnResposta = document.querySelector("#submit-btn")
+    btnResposta.addEventListener('click',responder)
+}
+function clearFields() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+  }
+  window.onload = function () {
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearFields);
+    const submitBtn = document.querySelector('#submit-btn');
+    submitBtn.addEventListener('click', handleSubmit);
+  };
+  
